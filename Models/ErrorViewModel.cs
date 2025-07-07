1ac251cf74c03a03ba40a7d68401c2bb132b866a
James <jamesfallouh@outlook.com>
2025-07-07 14:31:16 -0400

    dotnet new mvc -n ApFilterWebApp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ApFilterWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
